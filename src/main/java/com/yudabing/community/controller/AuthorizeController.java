c8b0c0d5fe1bfc986778750538e0e42643091bcb
David1126 <yubing1126@gmail.com>
2019-08-12 18:03:28 +0800

    提取常量到配置文件

--- src/main/java/com/yudabing/community/controller/AuthorizeController.java
+++ src/main/java/com/yudabing/community/controller/AuthorizeController.java
@@ -5,6 +5,7 @@ import com.yudabing.community.dto.AccessTokenDTO;
 import com.yudabing.community.dto.GithubUser;
 import com.yudabing.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,16 +21,23 @@ import java.io.IOException;
 public class AuthorizeController {
 
     @Autowired
-    GithubProvider githubProvider;
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String client_id;
+    @Value("${github.client.secret}")
+    private String client_secret;
+    @Value("${github.redirect.url}")
+    private String redirect_url;
 
     @RequestMapping("/callback")
     public String callback (@RequestParam(name = "state") String state,
                             @RequestParam(name = "code") String code) throws IOException {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("d27263e370d16a3188ce");
-        accessTokenDTO.setClient_secret("3cb4e7303fe6b3653be44a7b553b24555748a952");
+        accessTokenDTO.setClient_id(client_id);
+        accessTokenDTO.setClient_secret(client_secret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirect_url);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getGithubUser(accessToken);
